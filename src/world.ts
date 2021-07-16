9535afb5222088660c90e870c5ad5151af1aec77
Mayumi Ohnishi <kagura004@gmail.com>
2021-07-16 23:55:20 +0900

    test

--- src/world.ts
+++ src/world.ts
@@ -1,19 +1,15 @@
 export default class World {
   message: string;
   constructor(message: string) {
-    this.message = message
+    this.message = message;
   }
 
   public sayHello(elem: HTMLElement | null) {
     if (elem) {
-      elem.innerText = this.message
+      console.log('testメッセージ');
+
+      console.log('あああ');
+      elem.innerText = this.message;
     }
   }
-
-
-
-
-
-
-          
 }
