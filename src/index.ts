fc78646da024b36e9e063320d14319ed4acfddc0
Mayumi Ohnishi <kagura004@gmail.com>
2021-07-17 16:15:53 +0900

    [add]型定義の練習

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-import World from './world';
+// import World from './world';
+//
+// const root = document.getElementById('root');
+// const world = new World('Hello World!');
+//
+// world.sayHello(root);
 
-const root = document.getElementById('root');
-const world = new World('Hello World!');
+// 03. 基本の定義
 
-world.sayHello(root);
+// import { primitiveSample } from "./basic";
+// import { notExistSample } from "./basic";
+// import { anySample } from "./basic";
+// import { unknownSample } from "./basic";
+
+// primitiveSample();
+// notExistSample();
+// anySample();
+// unknownSample();
+
+// 04. 関数の型定義
+
+// import { logMessage } from "./function/basic"
+// logMessage("Hello TypeScript!");
+
+import { inUserSignedIn, inUserSignedIn2, sumProductsPrice } from './function/new';
+
+inUserSignedIn('ABC', 'mayumi');
+inUserSignedIn2('ABC');
+
+const sum: number = sumProductsPrice(100, 200, 300, 800, 304);
+console.log('Function parameters sample 5:', sum);
